c299221eb75d3eac63cf45cea354e07daaaebdb1
aajjiitt <ajit09rocks@gmail.com>
2022-01-25 05:40:20 +0530

    Added 3 new ml model code

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
+from traceback import print_tb
+
+
 print("Adding all ML related code here")
+
+def model1():
+    print("Working on mdel 1")
+
+def model2():
+    print("Working on mdel 2")  
+
+def model3():
+    print("Working on mdel 3")      
